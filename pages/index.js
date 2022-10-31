5404740a66c5e05b0832b129276a95a4cfdb6326
Sumeet Debnath <sumeetdeb100@gmail.com>
2022-10-31 21:36:57 +0530

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
-
+import Head from 'next/head'
+import Image from 'next/image'
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
 
 export default function Home() {
   return (
-    <>
-    <h1>Ankit kumar</h1>
-    </>
+
+
+    
+ <div className='relative'>
+  <Navbar/>
+<div className=''>
+  <Sidebar/>
+    <main className=' ml-64 pt-20 px-4'>
+      <h1 className='text-4xl'>content here</h1>
+    </main>
+</div>
+ </div>
   )
 }
